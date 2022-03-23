2e758ffc25c3b7180852487bebfe8443af49ebbe
Matheus Tanaka <matheustanakanog@gmail.com>
2022-03-22 22:32:59 -0300

    setup enviroment

---
+++ setupEnviroment/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<< "Hello World" <<endl;
+    cout << "Yup it's the outpout" <<endl;
+    return 0;
+}
---
+++ setupEnviroment/lab1/goodbye.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<< "Over and out" <<endl;
+    return 0;
+}
