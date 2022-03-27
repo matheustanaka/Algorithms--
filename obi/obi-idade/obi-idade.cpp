60297f1ab0f6fb358c31386b4700487b7bc33257
Matheus Tanaka <matheustanakanog@gmail.com>
2022-03-26 22:32:01 -0300

    obi-idade - wrong - was implemented the correct solution

--- obi/obi-idade/obi-idade.cpp
+++ obi/obi-idade/obi-idade.cpp
@@ -1,14 +1,22 @@
 #include<stdio.h>
 
+int mymax ( int x, int y) {
+    //using ternary operators ? = if   : = else
+    return (x >= y) ? x : y;
+}
+
 int main (void) {
 
-    int m, a, b;
+    int m, a, b, c;
 
     scanf("%d %d %d", &m, &a, &b);
 
-    if(m >= 40 && m <=110 && a >= 1 && a < m && b >= 1 && b < m && a != b) {
-        printf("%d\n", m - (a + b));
-    }
-
+    // if(m >= 40 && m <=110 && a >= 1 && a < m && b >= 1 && b < m && a != b) {
+    //     printf("%d\n", m - (a + b));
+    // }
+    // The comment is an example of a bad solution, i did it wrong, see the best solution below:
+    c = m - a - b;
+    //calling a function to check what's bigger than others.
+    printf("%d\n", mymax(mymax(a, b), c));
     return 0;
 }
