58d5a8577a4dfcd434d8a506cf9df0f1731eefd8
Matheus Tanaka <matheustanakanog@gmail.com>
2022-03-29 19:12:55 -0300

    obi-fliper - completed

---
+++ obi/obi-fliper/obi-fliper.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (void) {
+
+    int p, r;
+
+    scanf("%d %d", &p, &r);
+
+    if ( P == 0 && R == 0 ) printf("C\n");
+    if ( P == 0 && R == 1 ) printf("C\n");
+    if ( P == 1 && R == 0 ) printf("B\n");
+    if ( P == 1 && R == 1 ) printf("A\n");
+
+    return 0;
+}
