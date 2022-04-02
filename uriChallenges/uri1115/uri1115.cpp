d37d827ed6c8be66243103f0086bbcad2d1f1ea0
Matheus Tanaka <matheustanakanog@gmail.com>
2022-04-02 09:53:55 -0300

    uri1115 - completed

---
+++ uriChallenges/uri1115/uri1115.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main (void) {
+    
+    int x, y;
+
+    while (x != 0 || y != 0)
+    {
+        scanf("%d %d", &x, &y);
+        if(x > 0 && y > 0) {
+            printf("primeiro\n");
+        }
+        else if(x > 0 && y < 0) {
+            printf("quarto\n");
+        }
+        else if(x < 0 && y < 0) {
+            printf("terceiro\n");
+        }
+        else if(x < 0 && y > 0) {
+            printf("segundo\n");
+        }
+        else if(x == 0 || y == 0) {
+            break;
+        }
+    }
+
+    return 0;
+}
